e00bcd54a4e03031ba6d1757bd2f486802037860
Jeremy Hahn <mail@jeremyhahn.com>
2014-02-05 22:05:02 -0500

    mend

--- src/websocket/utf8.c
+++ src/websocket/utf8.c
@@ -1,24 +1,3 @@
-/**
- *  cwebsocket: A fast, lightweight websocket client/server
- *
- *  Copyright (c) 2014 Jeremy Hahn
- *
- *  This file is part of cwebsocket.
- *
- *  cwebsocket is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published
- *  by the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  cwebsocket is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with cwebsocket.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "utf8.h"
 
 int utf8_count_code_points(uint8_t* s, size_t* count) {
